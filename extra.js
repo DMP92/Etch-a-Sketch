ea1bc7f328a2786a6ef1244e5471366214521f47
Google Project <devin.pfromm.20@gmail.com>
2021-04-21 17:48:40 -0400

    Added the major features.
    - Grid
    - color schemes
    - customizable buttons

---
+++ extra.js
@@ -0,0 +1,21 @@
+
+
+/* 
+Randomized Color Scheme 
+
+function changeColor(e) {
+    
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    e.target.style.cssText = `background-color: rgba(${red}, ${green}, ${blue})`;
+    
+}
+color1.addEventListener('click', sunsetColor);
+color1.addEventListener('click', sunsetColor);
+
+let color1 = document.querySelector('colorOne');
+let color2 = document.querySelector('colorTwo');
+let color3 = document.querySelector('colorThree');
+
+*/
--- main.js
+++ main.js
@@ -1,36 +1,161 @@
+grid = document.createElement('div');
+grid.classList="grid";
+let etchGrid = document.getElementById('etchGrid');    
+let allGrids = document.querySelectorAll('.grid');
+let etchContainer = document.getElementById('etchContainer');
+let off = "off";
+let on = "on";
+var clicked1 = 'off';
+var clicked2 = 'off';
+var clicked3 = 'off';
+let standard = 'on';
 
-let etchGrid = document.getElementById('etchGrid');
+// Standard Grid and colors
 
+window.addEventListener('load', makeGrid(35));
 
-
-
-
-function gridCreation(num) {
-    for (let i = 1; i <= num; i++) {
-        const grid = document.createElement('div');
-        grid.classList="grid";
-        grid.addEventListener('mouseover', rGB);
-        etchGrid.appendChild(grid);  
-    }
-}
-gridCreation(100);
-        
-function rGB(e) {
-    let red = Math.floor(Math.random() * 256);
-    let green = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
-    let opacity = Math.random();
-    e.target.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-}
-rGB();
-
-
-
-
-    
-        
- 
+function makeGrid(cell) {
    
+    for (let i = 0; i < (cell * cell); i++) {
+        let grid = document.createElement('div');
+        grid.classList = 'grid';
+        
+        etchGrid.style.gridTemplateColumns = `repeat(${cell}, 1fr)`;
+        etchGrid.style.gridTemplateRows = `repeat(${cell}, 1fr)`;
+        etchGrid.appendChild(grid);
+    }
+
+}
 
 
+
+
+
+// Custom Grid Sizing
+
+let customButton = document.querySelector('.custom');
+customButton.onclick = () => {
+    gridPrompt();
+}
+
+function gridPrompt() {
+    cleanSlate();
+    let cell = prompt("Enter The Size Of Your Grid", "Any Number From 2 - 64");
+    if (cell <= 1 || cell > 65){
+        alert("Invalid Grid Size. (2 - 64 only)");
+    } else {
+        makeGrid(cell);
+    }
+} 
+
+
+
+// Color Scheme #1 
+//-----------------------------------------------
+let sunset = ['#212a51', '#404465', '#654e7a', '#944b76', '#b74b75', '#e86a6e', '#d06b65', '#e68a75', '#ee9c6c', '#ee9b6d', '#fdba5f',  '#f3d970', '#f3d970', '#f1ec9b', '#fdded9'];
+let firstColor = document.querySelector('.colorOne');
+
+firstColor.addEventListener('click', () => {
+    standard = off;
+    clicked1 = on;
+    clicked2 = off;
+    clicked3 = off;
+})
+
+
+
+
+
+// 'Earthy' - Color Scheme #2 
+//-----------------------------------------------
+let secondColor = document.querySelector('.colorTwo');
+
+let earthy = ['#403164', '#332354', '#232d60', ' #4c4799', '#66689b', '#252e4b', '#03568e', '#03568b', '#52b7c4',' #a4d3b4', '#4ca06f', '#89c291', '#a8d293', '#85c554', '#4da645'];
+
+function earthyColor(e) {
+    earth = Math.floor(Math.random() * earthy.length);
+    e.target.style.backgroundColor = `${earthy[earth]}`; 
+}  
+
+secondColor.addEventListener('click', () => {
+    standard = "off";
+    clicked1 = "off";
+    clicked2 = "on"; 
+    clicked3 = "off";
+    colorizeGrid;
+    
+})
+
+
+
+
+// 'Gray Scale' - Color Scheme #3
+//-----------------------------------------------
+let thirdColor = document.querySelector('.colorThree');
+
+
+
+function third() {
+    standard = 'off';
+    clicked1 = 'off';
+    clicked2 = 'off';
+    clicked3 = 'on';
+    colorizeGrid;
+}
+
+thirdColor.addEventListener('click', third);
+
+// Clears the grid 
+// ---------------------------------------------
+
+function cleanSlate() {
+    let allGrids = document.getElementsByClassName('grid');
+
+    for (let i = 0; i < allGrids.length; i++) {
+        allGrids[i].style.backgroundColor = 'transparent';
+    }    
+}    
+
+
+let clear = document.querySelector('.btnClear');
+clear.onclick = () => cleanSlate();
+
+
+
+// Logic for color scheme switches
+// ----------------------------------------------------------
+
+function colorizeGrid(e) {
+ 
+    if (clicked1 == "on") {
+        
+        sun = Math.floor(Math.random() * sunset.length);
+        e.target.style.backgroundColor = `${sunset[sun]}`;
+        
+     
+
+    }else if (clicked2 == "on") {
+        earth = Math.floor(Math.random() * earthy.length);
+        e.target.style.backgroundColor = `${earthy[earth]}`;
+     
+
+    } else if (clicked3 == "on") {
+        let gray = .1;
+        let shades = gray + .1;
+        e.target.style.backgroundColor = `rgba(0, 0, 0, ${shades + .1})`;
+     
+
+    } else {
+            let red = Math.floor(Math.random() * 256);
+            let green = Math.floor(Math.random() * 256);
+            let blue = Math.floor(Math.random() * 256);
+            e.target.style.backgroundColor = `rgba(${red}, ${green}, ${blue})`;
+         
+
+        }
+     
+}
+
+etchGrid.addEventListener('mouseover', colorizeGrid);
+    
 
